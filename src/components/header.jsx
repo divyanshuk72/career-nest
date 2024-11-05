494bb0d26587caa2a16e4f340fe5bc909c691df2
Kumar Divyanshu <87257292+divyanshuk72@users.noreply.github.com>
2024-11-05 23:11:29 +0530

    Added clerk dark theme and SignIn component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom";
 import Logo from "/Logo.png";
 import { Button } from "./ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 const Header = () => {
   return (
@@ -9,7 +15,13 @@ const Header = () => {
         <Link>
           <img src={Logo} alt="logo" className="h-24" />
         </Link>
-        <Button variant="outline">Login</Button>
+        {/* <Button variant="outline">Login</Button> */}
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </nav>
     </>
   );
