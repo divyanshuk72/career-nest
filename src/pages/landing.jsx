545a088cf304478a28264421ea5827ec5dfc5c89
Kumar Divyanshu <87257292+divyanshuk72@users.noreply.github.com>
2024-11-06 14:24:32 +0530

    Added Carousel

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,5 +1,12 @@
 import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
 import { Link } from "react-router-dom";
+import companies from "../data/companies.json";
+import Autoplay from "embla-carousel-autoplay";
 
 const LandingPage = () => {
   return (
@@ -28,6 +35,19 @@ const LandingPage = () => {
         </Link>
       </div>
       {/* Carousel  */}
+      <Carousel plugins={[Autoplay({ delay: 2000 })]} className="w-full py-10">
+        <CarouselContent className="flex gap-5 sm:gap-20 items-center">
+          {companies.map(({ name, id, path }) => (
+            <CarouselItem key={id} className="basis-1/3 lg:basis-1/6">
+              <img
+                src={path}
+                alt={name}
+                className="h-9 sm:h-14 object-contain"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
     </main>
   );
 };
