23ab93240014b35a9e6f1907f4ef8d1985b20d17
Kumar Divyanshu <87257292+divyanshuk72@users.noreply.github.com>
2024-11-06 13:22:44 +0530

    Background color change and landing page changes

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,5 +1,14 @@
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
+      <section className="text-center">
+        <h1 className="flex flex-col gap-4 justify-center items-center text-4xl sm:text-6xl lg:text-8xl font-extrabold">
+          Get your Dream Job
+          <span> @ CareerNest</span>
+        </h1>
+      </section>
+    </main>
+  );
 };
 
 export default LandingPage;
