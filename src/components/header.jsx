05d9f657517ecb12bae5c31a2cdd0e251e1b895e
Kumar Divyanshu <87257292+divyanshuk72@users.noreply.github.com>
2024-11-07 21:39:16 +0530

    Added conditional rendering for post job based on role

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,13 @@
 import { Link, useSearchParams } from "react-router-dom";
 import Logo from "/Logo.png";
 import { Button } from "./ui/button";
-import { SignedIn, SignedOut, SignIn, UserButton } from "@clerk/clerk-react";
+import {
+  SignedIn,
+  SignedOut,
+  SignIn,
+  UserButton,
+  useUser,
+} from "@clerk/clerk-react";
 import { BriefcaseBusiness, Heart, PenBox } from "lucide-react";
 import { useEffect, useState } from "react";
 
@@ -9,6 +15,8 @@ const Header = () => {
   const [showSignIn, setShowSignIn] = useState(false);
 
   const [search, setSearch] = useSearchParams();
+
+  const { user } = useUser();
 
   useEffect(() => {
     if (search.get("sign-in")) {
@@ -41,12 +49,14 @@ const Header = () => {
             </Button>
           </SignedOut>
           <SignedIn>
-            <Link to="/post-job">
-              <Button variant="destructive" className="rounded-full">
-                <PenBox size={20} className="mr-2" />
-                Post a Job
-              </Button>
-            </Link>
+            {user?.unsafeMetadata?.role === "recruiter" && (
+              <Link to="/post-job">
+                <Button variant="destructive" className="rounded-full">
+                  <PenBox size={20} className="mr-2" />
+                  Post a Job
+                </Button>
+              </Link>
+            )}
             <UserButton
               appearance={{
                 elements: {
