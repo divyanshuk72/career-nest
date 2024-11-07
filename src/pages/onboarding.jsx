1baa12eff2974f56503baf055cd69ddbd80e2802
Kumar Divyanshu <87257292+divyanshuk72@users.noreply.github.com>
2024-11-07 21:17:48 +0530

    Added react-spinners for loading animation

--- src/pages/onboarding.jsx
+++ src/pages/onboarding.jsx
@@ -1,4 +1,13 @@
+import { useUser } from "@clerk/clerk-react";
+import { BarLoader } from "react-spinners";
+
 const Onboarding = () => {
+  const { user, isLoaded } = useUser();
+
+  if (!isLoaded) {
+    return <BarLoader className="mb-4" width={"100%"} color="white" />;
+  }
+
   return <div>Onboarding</div>;
 };
 
