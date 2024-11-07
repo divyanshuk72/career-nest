d1d366ce6a4f1b42ef1d5ae82a0f262f8d0cd453
Kumar Divyanshu <87257292+divyanshuk72@users.noreply.github.com>
2024-11-07 21:33:38 +0530

    Role functionality added in onboarding

--- src/pages/onboarding.jsx
+++ src/pages/onboarding.jsx
@@ -1,11 +1,33 @@
 import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/clerk-react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { BarLoader } from "react-spinners";
 
 const Onboarding = () => {
   const { user, isLoaded } = useUser();
+  const navigate = useNavigate();
 
-  const handleRoleSelection = (role) => {};
+  const handleRoleSelection = async (role) => {
+    await user
+      .update({
+        unsafeMetadata: { role },
+      })
+      .then(() => {
+        navigate(role === "recruiter" ? "/post-job" : "/jobs");
+      })
+      .catch((err) => {
+        console.error("Error updating role : ", err);
+      });
+  };
+
+  useEffect(() => {
+    if (user?.unsafeMetadata?.role) {
+      navigate(
+        user?.unsafeMetadata?.role === "recruiter" ? "/post-job" : "/jobs"
+      );
+    }
+  }, [user]);
 
   if (!isLoaded) {
     return <BarLoader className="mb-4" width={"100%"} color="white" />;
